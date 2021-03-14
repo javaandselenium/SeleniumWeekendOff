17cbb9b52dbd8c817382ec45e85e955f44e833da
QSP <QSP@DESKTOP-EVK9GMC>
2021-03-14 10:32:41 +0530

    Poi

---
+++ src/popups/Test0.java
@@ -0,0 +1,27 @@
+package popups;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test0 {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","./software/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.javascriptkit.com/javatutors/alert2.shtml");
+		driver.findElement(By.xpath("//input[@value='Click here for output>>']")).click();
+		
+		Alert a = driver.switchTo().alert();
+		System.out.println(a.getText());
+		Thread.sleep(3000);
+		a.accept();
+		Thread.sleep(3000);
+		a.dismiss();
+		driver.close();
+
+	}
+
+}
---
+++ src/popups/Test2.java
@@ -0,0 +1,20 @@
+package popups;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","./software/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.naukri.com/account/register/basicdetails");
+		driver.findElement(By.xpath("//button[@title='I am a Professional']")).click();
+		driver.findElement(By.name("uploadCV")).sendKeys("C:\\Users\\QSP\\Documents\\Selenium 205\\Locators-w.docx");
+		
+
+	}
+
+}
